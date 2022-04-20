a98ac9082d98c2f60f01477eaf67ec8f66923786
임채성 <pan1129@naver.com>
2022-04-21 00:35:33 +0900

    게시판 CRUD Commit

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -30,4 +30,9 @@ public class PostsApiController {
     }
 
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+    }
 }
